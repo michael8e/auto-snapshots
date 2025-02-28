5a68ea9c3753636786dde99bebb41b2462dccf9c
Michael <8michael8e@gmail.com>
2025-02-28 19:34:36 +0200

    Update autosnap.php

--- autosnap.php
+++ autosnap.php
@@ -2,20 +2,8 @@
 <?PHP
 
 /*    AUTOSNAP.PHP - create snapshots of all your Digital Ocean droplets
- *    Copyright (C) 2014  Resultix BV  Marcel van Eck
+ *    Copyright (C) 2025  Michael
  *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with this program.  If not, see <http://www.gnu.org/licenses>
 */
 
 // Setup a cronjob at a "non-Digital-Ocean" machine to create automatic snapshots
